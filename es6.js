c8b8ea8948a221284a8b685d58771b89afd8b1f3
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-09-26 00:50:47 +0800

    Recap Nested Object Destructure

--- es6.js
+++ es6.js
@@ -5,5 +5,16 @@ const student={
     Id:2023030077
 }
 // Destructure value from object
-const {address,age,name:studentName,Id}=student;
-console.log(studentName,address,age,Id);
+// const {address,age,name:studentName,Id}=student;
+// // console.log(studentName,address,age,Id);
+// console.log(`Student Name: ${studentName}, Age: ${age}, ID: ${Id}, Address: ${address}`)
+
+
+// Nested Destructure
+const data={
+    profile:{
+        email:"useremail@gmail.com",
+        name:"Username"
+    }
+}
+console.log({profile}=data);
